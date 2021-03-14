c1815f8b94099298e4411624e084daac908a2dfb
matheusvllo <matheusvinicius38@live.com>
2021-03-14 20:08:17 -0300

    Project started

---
+++ src/main/java/com/wh/web_hospital/WebHospitalApplication.java
@@ -0,0 +1,13 @@
+package com.wh.web_hospital;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebHospitalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebHospitalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wh/web_hospital/WebHospitalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wh.web_hospital;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebHospitalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
