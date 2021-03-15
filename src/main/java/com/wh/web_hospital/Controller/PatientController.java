07c09ad23af66241904503d266eec9496527f8ec
matheusvllo <matheusvinicius38@live.com>
2021-03-15 18:03:30 -0300

    Post method now returns a 201 status

--- src/main/java/com/wh/web_hospital/Controller/PatientController.java
+++ src/main/java/com/wh/web_hospital/Controller/PatientController.java
@@ -7,12 +7,14 @@ import com.wh.web_hospital.Model.Patient;
 import com.wh.web_hospital.Repository.PatientRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -38,6 +40,7 @@ public class PatientController {
     }
 
     @PostMapping("/patients/new")
+    @ResponseStatus(HttpStatus.CREATED)
     public Patient newPatient(@RequestBody Patient patient){
         return patientRepository.save(patient);
     }
